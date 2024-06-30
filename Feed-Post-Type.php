64255b76f3f483f116c550aed133c2ef07aca700
Kevin Benabdelhak <163947458+kevinbenabdelhak@users.noreply.github.com>
2024-06-30 05:05:53 +0200

    Correction pour le type de contenu "post" (article)
    Permet de désactiver les articles du feed

--- Feed-Post-Type.php
+++ Feed-Post-Type.php
@@ -14,10 +14,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 class FeedPostType {
     
     public function __construct() {
+        register_activation_hook( __FILE__, array( $this, 'activate' ) );
         add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
         add_action( 'admin_init', array( $this, 'settings_init' ) );
         add_filter( 'request', array( $this, 'add_custom_post_type_to_feed' ) );
         add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+    }
+
+    public function activate() {
+        $default_options = array(
+            'post' => 1, // Init to 1 to check by default
+            // Initialize other default post types if necessary
+        );
+        
+        $existing_options = get_option( 'feed_post_type_settings' );
+        
+        if ( false === $existing_options ) {
+            update_option( 'feed_post_type_settings', $default_options );
+        }
     }
 
     public function add_admin_menu() {
@@ -52,9 +66,9 @@ class FeedPostType {
     }
 
     public function settings_field_callback( $args ) {
-        $options = get_option( 'feed_post_type_settings' );
+        $options = get_option( 'feed_post_type_settings', array() );
         ?>
-        <input type="checkbox" name="feed_post_type_settings[<?php echo $args['name']; ?>]" <?php checked( isset( $options[ $args['name'] ] ) ); ?> value="1">
+        <input type="checkbox" name="feed_post_type_settings[<?php echo $args['name']; ?>]" <?php checked( isset( $options[ $args['name'] ] ) && $options[ $args['name'] ] == 1 ); ?> value="1">
         <?php
     }
 
@@ -77,12 +91,13 @@ class FeedPostType {
             $options = get_option( 'feed_post_type_settings' );
 
             if ( ! empty( $options ) ) {
-                $post_types = array_keys( $options );
-                if ( ! isset( $qv['post_type'] ) ) {
+                $post_types = array_keys( array_filter( $options ) );
+                if ( ! empty( $post_types ) ) {
                     $qv['post_type'] = $post_types;
-                } else {
-                    $qv['post_type'] = array_merge( (array) $qv['post_type'], $post_types );
                 }
+            } else {
+                // Define empty post types to avoid default 'post' inclusion
+                $qv['post_type'] = array('none');
             }
         }
         return $qv;
@@ -95,4 +110,4 @@ class FeedPostType {
 
 new FeedPostType();
 
- ?>
+?>
