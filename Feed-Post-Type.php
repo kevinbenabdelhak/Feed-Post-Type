ebd5bf617546ee5be3ee1f14fd33e0570a73516d
Kevin Benabdelhak <163947458+kevinbenabdelhak@users.noreply.github.com>
2024-06-30 10:47:19 +0200

    MAJ avec ajout des liens des flux RSS personnalisés

--- Feed-Post-Type.php
+++ Feed-Post-Type.php
@@ -1,113 +1,115 @@
-<?php
+<?php 
+
 /**
  * Plugin Name: Feed Post Type
  * Plugin URI: https://kevin-benabdelhak.fr/plugins/feed-post-type/
  * Description: Permet de sélectionner les types de contenu à inclure dans les flux RSS.
- * Version: 1.0
+ * Version: 1.1
  * Author: Kevin BENABDELHAK
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; 
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 class FeedPostType {
-    
+
     public function __construct() {
-        register_activation_hook( __FILE__, array( $this, 'activate' ) );
-        add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
-        add_action( 'admin_init', array( $this, 'settings_init' ) );
-        add_filter( 'request', array( $this, 'add_custom_post_type_to_feed' ) );
-        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+        register_activation_hook(__FILE__, array($this, 'activate'));
+        add_action('admin_menu', array($this, 'add_admin_menu'));
+        add_action('admin_init', array($this, 'settings_init'));
+        add_action('pre_get_posts', array($this, 'filter_post_types_in_feed'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_styles'));
     }
 
     public function activate() {
-        $default_options = array(
-            'post' => 1, // Init to 1 to check by default
-            // Initialize other default post types if necessary
-        );
-        
-        $existing_options = get_option( 'feed_post_type_settings' );
-        
-        if ( false === $existing_options ) {
-            update_option( 'feed_post_type_settings', $default_options );
-        }
+        $default_options = array('post' => 1, 'page' => 1);
+        update_option('feed_post_type_settings', $default_options);
     }
 
     public function add_admin_menu() {
-        add_options_page( 'FeedPostType', 'FeedPostType', 'manage_options', 'feed_post_type', array( $this, 'options_page' ) );
+        add_options_page('FeedPostType', 'FeedPostType', 'manage_options', 'feed_post_type', array($this, 'options_page'));
     }
 
     public function settings_init() {
-        register_setting( 'pluginPage', 'feed_post_type_settings' );
+        register_setting('pluginPage', 'feed_post_type_settings');
 
         add_settings_section(
-            'feed_post_type_pluginPage_section', 
-            __( 'Sélectionnez les types de contenu à inclure dans le flux RSS:', 'feedposttype' ), 
-            array( $this, 'settings_section_callback' ), 
+            'feed_post_type_pluginPage_section',
+            __('Sélectionnez les types de contenu à inclure dans le flux RSS:', 'feedposttype'),
+            array($this, 'settings_section_callback'),
             'pluginPage'
         );
 
-        $post_types = get_post_types( array( 'public' => true ), 'objects' );
-        foreach ( $post_types as $post_type ) {
-            add_settings_field( 
+        $post_types = get_post_types(array('public' => true), 'objects');
+        foreach ($post_types as $post_type) {
+            add_settings_field(
                 'feed_post_type_' . $post_type->name,
                 $post_type->label,
-                array( $this, 'settings_field_callback' ),
+                array($this, 'settings_field_callback'),
                 'pluginPage',
                 'feed_post_type_pluginPage_section',
-                array( 'name' => $post_type->name )
+                array('name' => $post_type->name)
             );
         }
     }
 
     public function settings_section_callback() {
-        echo __( 'Cochez les types de contenu que vous souhaitez inclure dans le flux RSS:', 'feedposttype' );
+        echo __('Cochez les types de contenu que vous souhaitez inclure dans le flux RSS:', 'feedposttype');
     }
 
-    public function settings_field_callback( $args ) {
-        $options = get_option( 'feed_post_type_settings', array() );
+    public function settings_field_callback($args) {
+        $options = get_option('feed_post_type_settings', array());
         ?>
-        <input type="checkbox" name="feed_post_type_settings[<?php echo $args['name']; ?>]" <?php checked( isset( $options[ $args['name'] ] ) && $options[ $args['name'] ] == 1 ); ?> value="1">
+        <input type='checkbox' name='feed_post_type_settings[<?php echo $args['name']; ?>]' <?php checked(isset($options[$args['name']]) && $options[$args['name']] == 1); ?> value='1'>
+        <?php
+ 
+        $feed_url = get_feed_link() . '?post_type=' . $args['name'];
+        ?>
+        <small><a href="<?php echo esc_url($feed_url); ?>" target="_blank"><?php echo esc_html($feed_url); ?></a></small>
         <?php
     }
 
     public function options_page() {
         ?>
         <h1>FeedPostType</h1>
-
         <form action='options.php' method='post'>
-            <?php
-            settings_fields( 'pluginPage' );
-            do_settings_sections( 'pluginPage' );
+            <?php 
+            settings_fields('pluginPage');
+            do_settings_sections('pluginPage');
             submit_button();
             ?>
         </form>
         <?php
     }
 
-    public function add_custom_post_type_to_feed( $qv ) {
-        if ( isset( $qv['feed'] ) ) {
-            $options = get_option( 'feed_post_type_settings' );
+    public function filter_post_types_in_feed($query) {
+        if ($query->is_feed() && $query->is_main_query()) {
+            $requested_post_type = isset($_GET['post_type']) ? $_GET['post_type'] : '';
 
-            if ( ! empty( $options ) ) {
-                $post_types = array_keys( array_filter( $options ) );
-                if ( ! empty( $post_types ) ) {
-                    $qv['post_type'] = $post_types;
+            if ($requested_post_type) {
+        
+                if ($requested_post_type === 'page') {
+                    $query->set('post_type', 'page');
+                } else {
+                    $feed_options = get_option('feed_post_type_settings', array());
+                    if (isset($feed_options[$requested_post_type]) && $feed_options[$requested_post_type]) {
+                        $query->set('post_type', $requested_post_type);
+                    } else {
+                        $query->set('post_type', 'none');
+                    }
                 }
             } else {
-                // Define empty post types to avoid default 'post' inclusion
-                $qv['post_type'] = array('none');
+                $feed_options = get_option('feed_post_type_settings', array());
+                $valid_post_types = array_keys($feed_options, 1);
+                $query->set('post_type', $valid_post_types);
             }
         }
-        return $qv;
     }
 
     public function enqueue_styles() {
-        wp_enqueue_style( 'feedposttype-admin-style', plugins_url( 'style.css', __FILE__ ) );
+        wp_enqueue_style('feedposttype-admin-style', plugins_url('style.css', __FILE__));
     }
 }
 
 new FeedPostType();
-
-?>
